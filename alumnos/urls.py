7b1fd29f0586b6abbaefb5183bfc754c8ea9746c
russoleonardo88 <russoleonardo88@gmail.com>
2026-03-15 22:49:06 -0300

    Actualizacion diseño login y configuracion produccion

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,6 +1,12 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from alumnos.views import login_view, mi_rutina
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("mi-rutina/", views.mi_rutina),
+    path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
+    path('mi-rutina/', mi_rutina, name='mi_rutina'),
+    # Esto es por si quieres añadir un logout en el futuro
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- alumnos/views.py
+++ alumnos/views.py
@@ -1,21 +1,34 @@
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Alumno, Rutina
 
+# Vista para manejar el Login
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            # Redirige a la rutina tras iniciar sesión correctamente
+            return redirect('mi_rutina')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
 
+# Vista para la rutina del alumno
 @login_required
 def mi_rutina(request):
-
     alumno = Alumno.objects.get(user=request.user)
-
     dia_url = request.GET.get("dia")
 
     if dia_url:
         hoy_es = dia_url
     else:
         hoy = datetime.datetime.today().strftime("%A")
-
         dias = {
             "Monday": "Lunes",
             "Tuesday": "Martes",
@@ -23,7 +36,6 @@ def mi_rutina(request):
             "Thursday": "Jueves",
             "Friday": "Viernes",
         }
-
         hoy_es = dias.get(hoy)
 
     rutina_hoy = Rutina.objects.filter(alumno=alumno, nombre=hoy_es)
@@ -34,13 +46,10 @@ def mi_rutina(request):
 
     for rutina in rutina_hoy:
         for ejercicio in rutina.ejercicio_set.all():
-
             if ejercicio.tipo == "normal":
                 ejercicios_normal.append(ejercicio)
-
             elif ejercicio.tipo == "abdominal":
                 ejercicios_abdominal.append(ejercicio)
-
             elif ejercicio.tipo == "aerobico":
                 ejercicios_aerobico.append(ejercicio)
 
