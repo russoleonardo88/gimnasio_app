89af9e83bd10477fb36699b8475e9e21b9c4898f
russoleonardo88 <russoleonardo88@gmail.com>
2026-03-17 01:14:26 -0300

    Conexión de archivos estáticos en urls.py

--- gimnasio_app/urls.py
+++ gimnasio_app/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+# Importamos settings y la función static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('login/', auth_views.LoginView.as_view(template_name='login.html')),
-
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('', include('alumnos.urls')),
 ]
+
+# Esta es la conexión clave para que funcionen las imágenes y el CSS
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
